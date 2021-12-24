a19641d57f759528099999a4bd4b79d6e83c10eb
jahangir121988 <69716070+jahangir121988@users.noreply.github.com>
2021-12-24 07:28:44 +0500

    Create Utilities.php

---
+++ Utilities.php
@@ -0,0 +1,11 @@
+ 
+ ​<?php 
+ ​require_once​(​'constants.php'​); 
+  
+ ​function​ ​getConnection​() 
+ ​{ 
+ ​    ​$​dbh​ = ​new​ ​PDO​(​"mysql:host=localhost;"​, ​"root"​, ​""​); 
+ ​    ​$​dbh​->​exec​(​"USE "​ . ​DB​); 
+  
+ ​    ​return​ ​$​dbh​; 
+ ​}
